0c70b5c1313a31f55aa8457c359470677a4f839b
Sanju <rsanju.3312@gmail.com>
2026-05-13 13:53:04 +0530

    Fixed template directory for Render deployment

--- DjangoPro/Cravio/Cravio/settings.py
+++ DjangoPro/Cravio/Cravio/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Cravio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'delivery/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
